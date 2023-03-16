fb7dfa0849aff6f7422ca0d35b35ca449201ca1f
abirkholt <abirkholt@gmail.com>
2023-03-15 21:53:37 -0700

    coinflip

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,66 @@
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
+    
     // TODO: Add event listener and handler for flip and clear buttons
+    let total = 0;
+    let numberOfHeads = 0;
+    let numberOfTails = 0;
+    let percentageHeads = 0;
+    let percentageTails = 0;
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+    const flipButton = document.querySelector('#flip');
+    const clearButton = document.querySelector('#clear');
+    const statusMessage = document.querySelector('#statusMessage');
+    const pennyImage = document.querySelector('img');
+    const numberHeadCell = document.querySelector('#heads');
+    const numberTailscell = document.querySelector('#tails');
+    const percentageHeadsCell = document.querySelector('#heads-percent');
+    const percentageTailsCell = document.querySelector('#tails-percent');
 
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+    flipButton.addEventListener('click', function(e) {
+        total += 1;
+        console.log("hello")
+       
+        const result = Math.round(Math.random() * 100);
+        console.log(result)
+        // Heads
+
+        if (result > 50) {
+            numberOfHeads += 1;
+            statusMessage.textContent = 'You Flipped Heads!';
+            pennyImage.src = 'assets/images/penny-heads.jpg'; 
+            } else{
+            // Tails
+            numberOfTails +=1;
+            statusMessage.textContent = 'You Flipped Tails!';
+            pennyImage.src = 'assets/images/penny-tails.jpg';
+        }
+        percentageHeads = Math.round((numberOfHeads / total) * 100);
+        percentageTails = Math.round((numberOfTails / total) * 100);
+        numberHeadCell.textContent = `${numberOfHeads}`;
+        numberTailscell.textContent = `${numberOfTails}`;
+        percentageHeadsCell.textContent = `${percentageHeads}`;
+        percentageTailsCell.textContent = `${percentageTails}`;
+    });
+
+    clearButton.addEventListener('click', function(e) {
+        total = 0;
+        numberOfHeads = 0;
+        numberOfTails = 0;
+        percentageHeads = 0;
+        percentageTails = 0;
+
+        numberHeadCell.textContent = `${numberOfHeads}`;
+        numberTailscell.textContent = `${numberOfTails}`;
+        percentageHeadsCell.textContent = `${percentageHeads}`;
+        percentageTailsCell.textContent = `${percentageTails}`;
+        statusMessage.textContent = "Let's Get Rolling!";
+        pennyImage.src = 'assets/images/penny-heads.jpg';
 
 
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+
+        
+    });
 
 })
